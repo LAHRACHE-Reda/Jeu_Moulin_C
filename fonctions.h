55762f33f8222c87307dc989bda2107b05f8ad3a
LAHRACHE Reda <58349137+LAHRACHE-Reda@users.noreply.github.com>
2019-12-17 15:14:37 +0100

    Update fonctions.h

--- fonctions.h
+++ fonctions.h
@@ -2,10 +2,13 @@
 #define FONCTIONS_H_INCLUDED
 
 void Color(int couleurDuTexte,int couleurDeFond);
-void menu();
-void plateau();
-int posPion(tab,x);
-void regles();
+void menu(char T[24],char T2[24]);
+void plateau(char T[24],char T2[24]);
+//int posPion(tab,x);
+//void execute();
+int Player(int count1);
+void nbrPions(int count1);
+int condition(int count1, char s, char T[24],char T2[24]);
 
 
 #endif // FONCTIONS_H_INCLUDED
