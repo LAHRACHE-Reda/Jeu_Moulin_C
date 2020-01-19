d0edf42d95febd14e23ed81a987e920b2bfb825e
LAHRACHE Reda <58349137+LAHRACHE-Reda@users.noreply.github.com>
2020-01-19 14:24:22 +0100

    Update fonctions.h

--- fonctions.h
+++ fonctions.h
@@ -4,11 +4,14 @@
 void Color(int couleurDuTexte,int couleurDeFond);
 void menu(char T[24],char T2[24]);
 void plateau(char T[24],char T2[24]);
-//int posPion(tab,x);
+int posPion(char x,char T2[24]);
 //void execute();
 int Player(int count1);
 void nbrPions(int count1);
 int condition(int count1, char s, char T[24],char T2[24]);
+int MOULIN(char T2[24]);
+int cond_deplacer(char y ,char x ,char T[24] ,char T1[24]);
+void deplacer(char y, char x, char T[24], char T1[24]);
 
 
 #endif // FONCTIONS_H_INCLUDED
